dc0faa2c0f81316477ff0e39723899c9c8153437
P Reijerkerk <preijerkerk@gmail.com>
2018-10-24 18:37:41 -0400

    Created basic routes for displaying the json data as well as pushing the html to the user; created a basic express server and added in a variable port setup for use with heroku

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 //Dependencies
-let express = require('express');
-let path = require('path');
+let express = require("express");
+
+//Communicates with node that a "Express" server is being created
+let app = express();
+
+//Sets up the initial port using 3000 as localhost or setting value to the port assigned by heroku
+let PORT = process.env.PORT || 3000;
+
+//Sets up the "Express" server to handle data parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+//Points the server to the route files
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+//Listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
