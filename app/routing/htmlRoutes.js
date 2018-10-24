90401e55c589c21b5190ebab9058a98815edd996
P Reijerkerk <preijerkerk@gmail.com>
2018-10-24 19:57:51 -0400

    Fixed an error for the html routing to allow home.html to display

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,11 +4,11 @@ let path = require('path');
 //Routing
 module.exports = function(app) {
     app.get("*", function(req, res) {
-        res.sendfile(path.join(__dirname, "../app/public/home.html"));
+        res.sendfile(path.join(__dirname, "../public/home.html"));
     });
 
     app.get("/survey", function(req, res) {
-        res.sendfile(path.join(__dirname, "../app/public/survey.html"));
+        res.sendfile(path.join(__dirname, "../public/survey.html"));
     });
 };
 
