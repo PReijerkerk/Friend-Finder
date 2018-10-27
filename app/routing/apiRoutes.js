2de191651371543c4b1d267886f93366fcb654c8
P Reijerkerk <preijerkerk@gmail.com>
2018-10-27 17:39:26 -0400

    Changed the path for posting req.body to allow the route to compare user scores and return bffindex

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,7 +8,33 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res) {
-        friendsData.push(req.body);
-        res.json(true);
+        //friendsData.push(req.body);
+        //res.json(true);
+        let user = req.body;
+
+        for (var i = 0; i < user.scores.length; i++) {
+            user.scores[i] = parseInt(user.scores[i]);
+        }
+
+
+        let bffIndex = 0;
+        let maxDifference = 40;
+
+        for (var i = 0; i < friendsData.length; i++) {
+            let totalDifference = 0;
+            for (var j = 0; j < friendsData[i].scores.length; j++) {
+                let difference = Math.abs(user.scores[j] - friendsData[i].scores[j]);
+                totalDifference += difference;
+            }
+
+            if(totalDifference < maxDifference) {
+                bffIndex = i;
+                maxDifference = totalDifference;
+            }
+        }
+        
+        friendsData.push(user);
+
+        res.json(friendsData[bffIndex]);
     });
 };
