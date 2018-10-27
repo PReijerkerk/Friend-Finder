1204672be6930112071dec68201f1057227ba6db
P Reijerkerk <preijerkerk@gmail.com>
2018-10-27 11:31:06 -0400

    Updated the friends.js to have the default friends scores as strings instead of integers

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,32 +3,32 @@ let userData = [
         "name":"Ahmed",
         "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
         ]
     },
     {
         "name":"Peter",
         "photo":"https://preijerkerk.github.io/assets/images/biopicture.jpg",
         "scores":[
-            3,
-            1,
-            2,
-            4,
-            3,
-            1,
-            2,
-            2,
-            4,
-            1
+            "3",
+            "1",
+            "2",
+            "4",
+            "3",
+            "1",
+            "2",
+            "2",
+            "4",
+            "1"
         ]
     }
 ];
